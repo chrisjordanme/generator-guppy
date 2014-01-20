6cc7313519bb3dfdbea437162030286489b503e2
Chris Jordan <cljordan1@gmail.com>
2014-01-20 05:54:00 -0500

    Added build task to begin populating dist folder

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -7,6 +7,7 @@ var concat   = require('gulp-concat');
 var uglify   = require('gulp-uglify');
 var minCSS   = require('gulp-minify-css');
 var jsHint   = require('gulp-jshint');
+var rename   = require('gulp-rename');
 
 // Server
 var http     = require('http');
@@ -23,10 +24,17 @@ var host     = 'localhost';
 var protocol = 'http';
 var dist     = '/dist/';
 
-// todo-cj : make livereload a thing
+// todo-cj : add livereload
+// todo-cj : add sass compiler
 
 gulp.task('default', function () {
     gulp.run('server');
+});
+
+gulp.task('build', function () {
+    gulp.run('jshint');
+    gulp.run('minify-css');
+    gulp.run('uglify');
 });
 
 gulp.task('server', function () {
@@ -36,28 +44,27 @@ gulp.task('server', function () {
     open(protocol + '://' + host + ':' + port);
 });
 
-// todo-cj : roll a 'build' task..
-gulp.task('scripts', function () {
-    gulp.src('./app/scripts/*.js')
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('./dist/scripts'))
-});
-
 gulp.task('uglify', function () {
     gulp.src('./app/scripts/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/scripts/ugly'))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./dist/scripts'))
+        .pipe(rename('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts'))
 });
 
 gulp.task('minify-css', function () {
     var opts = {};
     gulp.src('./app/styles/*.css')
-    .pipe(minCSS(opts))
-    .pipe(gulp.dest('./dist/styles'))
+        .pipe(minCSS(opts))
+        .pipe(gulp.dest('./dist/styles'))
+        // todo-cj : get min rename working
+        .pipe(rename('styles.min.css'))
+        .pipe(gulp.dest('./dist/styles'))
 });
 
-// todo-cj : broken.. :(
 gulp.task('jshint', function () {
     gulp.src('./app/scripts/*.js')
-    .pipe(jsHint())
+        .pipe(jsHint())
+        .pipe(jsHint.reporter('default'))
 });
